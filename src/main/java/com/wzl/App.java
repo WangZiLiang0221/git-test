cf0fde80d672cea8dae26367905fa32f1099a785
wangziliang <wangziliang0221@163.com>
2023-03-09 21:06:40 +0800

    idea first hot-fix commit

--- src/main/java/com/wzl/App.java
+++ src/main/java/com/wzl/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hello git");
         System.out.println("second commit");
+        System.out.println("hot-fix test");
     }
 }
