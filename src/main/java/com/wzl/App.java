a294b509f54553affa91107e7b2a24cf6a1d3edc
wangziliang <wangziliang0221@163.com>
2023-03-09 20:57:59 +0800

    idea first commit

---
+++ src/main/java/com/wzl/App.java
@@ -0,0 +1,14 @@
+package com.wzl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hello git");
+    }
+}
